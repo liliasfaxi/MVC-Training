e90a1663a5da6f91b9609b93505a903beae01cd8
Graham Meehan <graham_meehan@hotmail.com>
2013-01-24 17:03:36 -0500

    Page 104 - Starting Chapter 5

---
+++ Pro ASP.NET MVC 4 Book/WorkingWithRazor/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using WorkingWithRazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+    }
+}
