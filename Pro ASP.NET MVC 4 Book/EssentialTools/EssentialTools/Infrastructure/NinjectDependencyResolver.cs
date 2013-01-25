80ad4da081d745f413a802f5dcd64d586f57c371
Graham Meehan <graham_meehan@hotmail.com>
2013-01-25 14:08:54 -0500

    Page 146

---
+++ Pro ASP.NET MVC 4 Book/EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Infrastructure {
+    public class NinjectDependencyResolver : IDependencyResolver {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver() {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService( Type serviceType ) {
+            return kernel.TryGet( serviceType );
+        }
+
+        public IEnumerable<object> GetServices( Type serviceType ) {
+            return kernel.GetAll( serviceType );
+        }
+
+        private void AddBindings() {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            //kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M); //to pass property into constructor
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument( "discountParam", 50M );
+            kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedInto<LinqValueCalculator>();
+        }
+    }
+}
---
+++ Pro ASP.NET MVC 4 Book/EssentialTools/EssentialTools/Models/FlexibleDiscountHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models {
+    public class FlexibleDiscountHelper : IDiscountHelper {
+
+        //applies different discounts based on the magnitude of the total being discounted
+        public decimal ApplyDiscount( decimal totalParam ) {
+            decimal discount = totalParam > 100 ? 70 : 25;
+            return ( totalParam - ( discount / 100m * totalParam ) );
+        }
+
+    }
+}
