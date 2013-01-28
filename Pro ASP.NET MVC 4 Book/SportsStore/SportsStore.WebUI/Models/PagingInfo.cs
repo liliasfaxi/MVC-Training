ce50b42229dc521cae3c3939db389dbf9c4fd600
Graham <graham_meehan@hotmail.com>
2013-01-27 21:24:57 -0500

    Almost done chapter 7 (pg. 193)

---
+++ Pro ASP.NET MVC 4 Book/SportsStore/SportsStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SportsStore.WebUI.Models {
+
+    //A view model is not part of our domain model, it is just a convenient class for passing data between controller and view
+    public class PagingInfo {
+
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
